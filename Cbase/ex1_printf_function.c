9c663e06944ad9e8cc6ed6f2995e738006ee7655
wanglixuan2407 <1924162379@qq.com>
2024-07-13 15:14:49 +0800

    7.13

--- Cbase/ex1_printf_function.c
+++ Cbase/ex1_printf_function.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 int main()
 {
+    //This is a comment
+    
+    /*
+    This 
+    is
+    a
+    multi-line
+    comment
+    */
+
     int a = 1;
     printf("a = %d\n",a);
+    printf("1\t2\t3\t4\t5\n");
     return 0;
 }
